ae675d9c97f32e171372c1f3cf0ba44562593df1
chini5ko <chinisko@outlook.com>
2020-03-05 23:52:08 -0500

    implemented hover on image

--- my-app/src/components/EachGifContainer.js
+++ my-app/src/components/EachGifContainer.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react'
 import style from './EachGifContainer.module.css'
 
 class EachGifContainer extends Component {
+
+    openNewTabForGif(url) {
+        window.open(url, '_blank');
+    }
+
     render() {
         return (
-                <img src={this.props.gifURL} key={this.props.downsizedURL} alt='gif' 
+                <img src={this.props.gifURL} key={this.props.index} alt='gif' onClick={() => this.openNewTabForGif(this.props.gifURL)} 
                 className={`${style.imgSize} ${style.cursor}  ${style.gifInteraction}`}  ></img>
         )
     }

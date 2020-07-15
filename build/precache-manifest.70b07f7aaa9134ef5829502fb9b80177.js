ad1382ee2b4a025c6d3368083fbc570b5547e268
chini5ko <chinisko@outlook.com>
2020-07-15 18:55:57 -0400

    added build to main folder

---
+++ build/precache-manifest.70b07f7aaa9134ef5829502fb9b80177.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db04af8989ce3dca61cf82bc0fa8860a",
+    "url": "/marvelChallenge/index.html"
+  },
+  {
+    "revision": "f58dc9ad59fb0c1f2dd8",
+    "url": "/marvelChallenge/static/css/main.01d9ebe8.chunk.css"
+  },
+  {
+    "revision": "171bc9c74f5716a859c7",
+    "url": "/marvelChallenge/static/js/2.90c15f6e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/marvelChallenge/static/js/2.90c15f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58dc9ad59fb0c1f2dd8",
+    "url": "/marvelChallenge/static/js/main.01dc1971.chunk.js"
+  },
+  {
+    "revision": "dc18b27c45d79cccf728",
+    "url": "/marvelChallenge/static/js/runtime-main.ff911f3e.js"
+  },
+  {
+    "revision": "e5e0f05a13307710f2d2b830ff0193eb",
+    "url": "/marvelChallenge/static/media/marvelSearchIcon.e5e0f05a.svg"
+  },
+  {
+    "revision": "b8caa593c5fa03e9ffaec39a7bb86c13",
+    "url": "/marvelChallenge/static/media/marvelSearchIconHover.b8caa593.svg"
+  }
+]);
